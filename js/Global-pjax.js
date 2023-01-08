84937167f4ef91b769b6742bac6a54bf71a6f2ae
acozycotage <admin@acozycotage.net>
2023-01-09 07:46:47 +0800

    Site updated: 2023-01-09 07:46:46

--- js/Global-pjax.js
+++ js/Global-pjax.js
@@ -1,3 +1,6 @@
+function whenDOMReady() {
+    // pjax加载完成（切换页面）后需要执行的函数和代码
+  }
 // 全局猫咪挂绳
 if (document.body.clientWidth > 992) {
     function getBasicInfo() {
@@ -440,3 +443,9 @@ var leonus = {
         })
     },
 };
+
+whenDOMReady() // 打开网站先执行一次
+document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
+
+// whenDOMReady函数外放一些打开网站之后只需要执行一次的函数和代码，比如一些监听代码。
+// 监听代码只需要执行一次即可，不需要每次加载pjax都执行，会出现一些Bug。至于为什么，我也不知道，可以自己试一下。
