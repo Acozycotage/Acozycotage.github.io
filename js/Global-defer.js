92987e340c7efb577af45ccd80c3421d8a0380fe
acozycotage <admin@acozycotage.net>
2023-01-11 17:35:43 +0800

    Site updated: 2023-01-11 17:35:42

--- js/Global-defer.js
+++ js/Global-defer.js
@@ -1,13 +1,12 @@
 // 全局deferJS存放
-
 var now = new Date();
 function createtime() {
   // 当前时间
   now.setTime(now.getTime() + 1000);
-  var start = new Date("08/01/2022 00:00:00"); // 旅行者1号开始计算的时间
+  var start = new Date("01/01/2023 00:00:00"); // 旅行者1号开始计算的时间
   var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
   var unit = (dis / 149600000).toFixed(6);  // 天文单位
-  var grt = new Date("08/09/2022 00:00:00");	// 网站诞生时间
+  var grt = new Date("01/01/2023 00:00:00");	// 网站诞生时间
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
     dnum = Math.floor(days),
     hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
@@ -19,7 +18,7 @@ function createtime() {
   var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
     snum = Math.round(seconds);
   1 == String(snum).length && (snum = "0" + snum);
-  let currentTimeHtml = "";
+  let currentTimeHtml = "01/01/2023 00:00:00";
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
     ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
@@ -27,4 +26,7 @@ function createtime() {
     document.getElementById("workboard") &&
     (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
-
+// 设置重复执行函数，周期1000ms
+setInterval(() => {
+  createtime("01/01/2023 00:00:00");
+}, 1000);c
