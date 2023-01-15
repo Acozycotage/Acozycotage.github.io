d747017dfc9d66aea9a2521a84d3fa2c27ccf675
acozycotage <admin@acozycotage.net>
2023-01-15 22:55:13 +0800

    Site updated: 2023-01-15 22:55:12

--- js/Global-pjax.js
+++ js/Global-pjax.js
@@ -227,73 +227,6 @@ window.onload = showWelcome;
 document.addEventListener('pjax:complete', showWelcome);
 // 如果使用了pjax在加上下面这行代码
 
-//导航栏魔改增强版
-document.addEventListener('pjax:complete', tonav);
-document.addEventListener('DOMContentLoaded', tonav);
-//响应pjax
-function tonav() {
-    document.getElementById("name-container").setAttribute("style", "display:none");
-    var position = $(window).scrollTop();
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if (scroll > position) {
-            document.getElementById("name-container").setAttribute("style", "");
-            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
-        } else {
-            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
-            document.getElementById("name-container").setAttribute("style", "display:none");
-        }
-        position = scroll;
-    });
-    //修复没有弄右键菜单的童鞋无法回顶部的问题
-    document.getElementById("page-name").innerText = document.title.split(" | Fomalhaut🥝")[0];
-}
-
-function scrollToTop() {
-    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
-    document.getElementById("name-container").setAttribute("style", "display:none");
-    btf.scrollToDest(0, 500);
-}
-
-var leonus = {
-    linkCom: e => {
-        var t = document.querySelector(".el-textarea__inner");
-        "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "站点名称：\n站点地址：\n头像链接：\n站点描述：\n站点截图：", t.setSelectionRange(5, 5)), t.focus()
-    },
-    owoBig: () => {
-        if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
-        let e = 1,
-            t = "",
-            o = document.createElement("div"),
-            n = document.querySelector("body");
-        o.id = "owo-big", n.appendChild(o), new MutationObserver((l => {
-            for (let a = 0; a < l.length; a++) {
-                let i = l[a].addedNodes,
-                    s = "";
-                if (2 == i.length && "OwO-body" == i[1].className) s = i[1];
-                else {
-                    if (1 != i.length || "tk-comment" != i[0].className) continue;
-                    s = i[0]
-                }
-                s.onmouseover = l => {
-                    e && ("OwO-body" == s.className && "IMG" == l.target.tagName || "tk-owo-emotion" == l.target.className) && (e = 0, t = setTimeout((() => {
-                        let e = 3 * l.path[0].clientHeight,
-                            t = 3 * l.path[0].clientWidth,
-                            a = l.x - l.offsetX - (t - l.path[0].clientWidth) / 2,
-                            i = l.y - l.offsetY;
-                        a + t > n.clientWidth && (a -= a + t - n.clientWidth + 10), a < 0 && (a = 10), o.style.cssText = `display:flex; height:${e}px; width:${t}px; left:${a}px; top:${i}px;`, o.innerHTML = `<img src="${l.target.src}">`
-                    }), 300))
-                }, s.onmouseout = () => {
-                    o.style.display = "none", e = 1, clearTimeout(t)
-                }
-            }
-        })).observe(document.getElementById("post-comment"), {
-            subtree: !0,
-            childList: !0
-        })
-    },
-};
-
 // 新年卡片
 let newYearTimer = null;
 var newYear = () => {
@@ -446,6 +379,7 @@ function createtime() {
     document.getElementById("workboard") &&
     (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
+
 // 设置重复执行函数，周期1000ms
 setInterval(() => {
   createtime("01/01/2023 00:00:00");
@@ -846,3 +780,70 @@ function switchNightMode() {
     typeof FB === 'object' && window.loadFBComment()
     window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
 }
+
+//导航栏魔改增强版
+document.addEventListener('pjax:complete', tonav);
+document.addEventListener('DOMContentLoaded', tonav);
+//响应pjax
+function tonav() {
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    var position = $(window).scrollTop();
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+            document.getElementById("name-container").setAttribute("style", "");
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+        } else {
+            document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+            document.getElementById("name-container").setAttribute("style", "display:none");
+        }
+        position = scroll;
+    });
+    //修复没有弄右键菜单的童鞋无法回顶部的问题
+    document.getElementById("page-name").innerText = document.title.split(" | Fomalhaut🥝")[0];
+}
+
+function scrollToTop() {
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+    btf.scrollToDest(0, 500);
+}
+
+var leonus = {
+    linkCom: e => {
+        var t = document.querySelector(".el-textarea__inner");
+        "bf" == e ? (t.value = "```yml\n", t.value += "- name: \n  link: \n  avatar: \n  descr: \n  siteshot: ", t.value += "\n```", t.setSelectionRange(15, 15)) : (t.value = "站点名称：\n站点地址：\n头像链接：\n站点描述：\n站点截图：", t.setSelectionRange(5, 5)), t.focus()
+    },
+    owoBig: () => {
+        if (!document.getElementById("post-comment") || document.body.clientWidth < 768) return;
+        let e = 1,
+            t = "",
+            o = document.createElement("div"),
+            n = document.querySelector("body");
+        o.id = "owo-big", n.appendChild(o), new MutationObserver((l => {
+            for (let a = 0; a < l.length; a++) {
+                let i = l[a].addedNodes,
+                    s = "";
+                if (2 == i.length && "OwO-body" == i[1].className) s = i[1];
+                else {
+                    if (1 != i.length || "tk-comment" != i[0].className) continue;
+                    s = i[0]
+                }
+                s.onmouseover = l => {
+                    e && ("OwO-body" == s.className && "IMG" == l.target.tagName || "tk-owo-emotion" == l.target.className) && (e = 0, t = setTimeout((() => {
+                        let e = 3 * l.path[0].clientHeight,
+                            t = 3 * l.path[0].clientWidth,
+                            a = l.x - l.offsetX - (t - l.path[0].clientWidth) / 2,
+                            i = l.y - l.offsetY;
+                        a + t > n.clientWidth && (a -= a + t - n.clientWidth + 10), a < 0 && (a = 10), o.style.cssText = `display:flex; height:${e}px; width:${t}px; left:${a}px; top:${i}px;`, o.innerHTML = `<img src="${l.target.src}">`
+                    }), 300))
+                }, s.onmouseout = () => {
+                    o.style.display = "none", e = 1, clearTimeout(t)
+                }
+            }
+        })).observe(document.getElementById("post-comment"), {
+            subtree: !0,
+            childList: !0
+        })
+    },
+};
